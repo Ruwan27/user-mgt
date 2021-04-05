f7d8a394d79ace9cd4a0372cd0e64af1445070d1
Ruwan Liyanage <liyanage.ruwan27@gmail.com>
2021-04-05 09:50:24 +0530

    initial commit

---
+++ src/main/java/lk/demo/app/usermgt/UserMgtApplication.java
@@ -0,0 +1,13 @@
+package lk.demo.app.usermgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/demo/app/usermgt/UserMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.demo.app.usermgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
